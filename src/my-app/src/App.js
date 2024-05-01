167ea918ae7eb338bc89209ab4309dc99b9e6624
Sudip Dhua <154453399+Sudip03Dhua@users.noreply.github.com>
2024-05-01 13:33:11 +0000

    added

--- src/my-app/src/App.js
+++ src/my-app/src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { ToastContainer, toast } from 'react-toastify';
+// import { ToastContainer, toast } from 'react-toastify';
 import { Button } from 'reactstrap';
 
 function App() {
   
-    const notify = () => toast("Wow so easy !");
+    // const notify = () => toast.success("Wow so easy !",{
+    //   position:'top-left'
+    // });
 
   return (
-    <div className="App">
-     <Button onClick={notify} color='primary'>Hello</Button>
-     <ToastContainer/>
+    <div className="">
+     {/* <Button onClick={notify} color='primary'>Hello</Button>
+     <ToastContainer/> */}
+
+        <h1 className="display-3">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-2" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+    
     </div>
   );
 }
